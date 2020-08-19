8b11fb913506ecd1fd4a610f50eb99373eae571e
RaphaelRPera <raphael.r.pera@gmail.com>
2020-08-19 17:47:02 -0300

    raphael - CardProduto e Home

---
+++ src/Home/Home.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import styled from 'styled-components'
+import { CardProduto } from '../CardProduto/CardProduto'
+
+
+const HomeBox = styled.main`
+    box-sizing: border-box;
+    border: 1px solid black;
+    /* min-height: 90vh; */
+    min-width: 300px;
+    background-color: gray;
+    display: flex;
+    justify-content: flex-start;
+    flex-wrap: wrap;
+`
+
+
+export class Home extends React.Component {
+    state = {
+        produtosHome: [],
+        produtosCarrinho: [],
+        prodID: 0,
+    }
+
+    adicionarCarrinho = (id) => {
+        console.log("Adicionar Carrinho", id)
+    }
+
+    render() {
+        {this.state.produtos.length <= 0 && this.setState({produtosHome: [{id: this.state.prodID + 1, name: 'Nome do Produto', imgUrl:'https://picsum.photos/200/150', price: 219.9}], prodID: this.state.prodID + 1})}
+        
+        let produtosNova
+        if(this.state.produtosHome.length > 0){
+            produtosNova = this.state.produtos.map((produto)=>{
+                return(
+                    <CardProduto
+                        key={produto.id}
+                        prodId={produto.id}
+                        prodImg={produto.imgUrl}
+                        prodNome={produto.name}
+                        prodPreco={produto.price}
+                        adicionarCarrinho={()=>this.adicionarCarrinho(produto.id)}
+                    ></CardProduto>
+                )
+            })
+        }
+
+        return(
+            <HomeBox>
+                {produtosNova}
+            </HomeBox>
+        )
+    }
+}
