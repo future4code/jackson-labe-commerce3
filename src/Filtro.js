73aea7380e8dd6f008fdcda8d0c04f72db380ad9
unknown <tbrito495@gmail.com>
2020-08-19 14:34:44 -0700

    filtro incompleto

---
+++ src/Filtro.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import styled from "styled-components";
+const FiltroEntreLinhas = styled.div`
+  margin: 10px 0;
+`
+const FiltroGeral = styled.div`
+position: fixed;
+width: 10vw; 
+height:100%;
+padding:10px;
+text-align: left;
+border:2px solid;
+`
+
+class Filtro extends React.Component{
+    state ={
+        Filtros:[
+            {
+            valorMinimo:"",
+            valorMaximo:"",
+            produto:""
+
+            }
+
+        ],
+        inputMinimo:"",
+        inputMaximo:"",
+        inputProduto:""
+    };
+    
+    adicionaValor = () =>{
+        const novoValor = {
+            valorMinimo:this.state.inputMinimo,
+            valorMaximo:this.state.inputMaximo,
+            produto:this.state.inputProduto
+        };
+        const novosFiltros = [...this.state.Filtros, novoValor];
+        this.setState({filtros:novosFiltros});
+    }
+        
+
+    render(){
+
+        return(
+
+
+            <FiltroGeral>
+                <h1>Filtros</h1>
+                <FiltroEntreLinhas>
+                <label>Valor Mínimo</label>
+                <input type="number"></input>
+                </FiltroEntreLinhas>
+                <FiltroEntreLinhas>
+                <label>Valor Máximo</label>
+                <input type="number"></input>
+                </FiltroEntreLinhas>
+                <FiltroEntreLinhas>
+                <label>Buscar Produto</label>
+                <input type="text"></input>
+                </FiltroEntreLinhas>
+            </FiltroGeral>
+
+
+        )
+    }
+
+}
+export default Filtro;
+
+
+
+
+
